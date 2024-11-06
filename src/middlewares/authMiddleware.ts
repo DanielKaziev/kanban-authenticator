2011a73d1ab58c613c5e9acd11f5e2a78de02dc4
Daniel Kaziev <kaziev30@mail.ru>
2024-11-07 00:05:27 +0500

    fix: auth middleware

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -3,25 +3,33 @@ import { RequestError } from "../utils/errors";
 import { EUserResponse } from "../config/messages";
 import tokenService from "../service/tokenService";
 
-const errorMiddleware = (req: Request, res: Response, next: NextFunction) => {
+const authMiddleware = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   try {
     if (req.method === "OPTIONS") return next();
 
     const authorization = req.headers.authorization;
-    if (!authorization)
+    if (!authorization) {
       return next(RequestError.Unauthorized(EUserResponse.NO_TOKEN));
+    }
 
     const token = authorization.split(" ")[1];
-    if (!token) next(RequestError.Unauthorized(EUserResponse.NO_TOKEN));
+    if (!token) {
+      return next(RequestError.Unauthorized(EUserResponse.NO_TOKEN));
+    }
 
-    const userData = tokenService.validateAccessToken(token);
-    if (!userData)
+    const userData = await tokenService.validateAccessToken(token)
+    if (!userData) {
       return next(RequestError.Unauthorized(EUserResponse.TOKEN_INVALID));
+    }
 
     next();
   } catch (err) {
-    return next(RequestError.Unauthorized(EUserResponse.UNAUTHORIZED_USER));
+    next(RequestError.Unauthorized(EUserResponse.UNAUTHORIZED_USER));
   }
 };
 
-export default errorMiddleware;
+export default authMiddleware;
